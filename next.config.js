4732cde4240c3017db987eb578ca7f20f5193b64
Ubuntu <ubuntu@ip-172-31-42-177.us-east-2.compute.internal>
2026-03-04 09:07:39 +0000

    Add Vercel config and static export settings

--- next.config.js
+++ next.config.js
@@ -1,29 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'dist',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.notion.so',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.gumroad.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.filepicker.io',
-      },
-    ],
     unoptimized: true,
   },
 }
