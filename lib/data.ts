ee461fb431aba50e5b7e16bbeb8d9fda89dea375
Ubuntu <ubuntu@ip-172-31-42-177.us-east-2.compute.internal>
2026-03-09 04:59:20 +0000

    Fix prices: Newsletter 9, DB Blueprint 9, Claude Skills

--- lib/data.ts
+++ lib/data.ts
@@ -43,7 +43,7 @@ const allProducts: NotionItem[] = [
     imageUrl: 'https://placehold.co/400x300/FDFBF7/1A1A1A?text=Newsletter+Playbook',
     category: 'Template',
     link: 'https://curioustapan.gumroad.com/l/thyra',
-    price: '$1'
+    price: '$19'
   },
   {
     id: 'g4',
@@ -52,7 +52,7 @@ const allProducts: NotionItem[] = [
     imageUrl: 'https://placehold.co/400x300/FDFBF7/1A1A1A?text=No+DB+Blueprint',
     category: 'Template',
     link: 'https://curioustapan.gumroad.com/l/tiraw',
-    price: '$1'
+    price: '$19'
   },
   {
     id: 'g5',
